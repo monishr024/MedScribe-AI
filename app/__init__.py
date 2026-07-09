e0da551089f70d2d5e022bd078e10484986637e9
Monish R <monishr024@gmail.com>
2026-07-10 03:01:22 +0530

    Config changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,13 @@ from app.config import Config
 
 
 def create_app():
-    """
-    Application Factory for MedScribe AI.
-    """
 
     app = Flask(__name__)
 
-    app.secret_key = "medscribe_ai_secret"
-
-    # Load configuration
     app.config.from_object(Config)
 
-    # Register blueprints
     from app.routes.main import main_bp
+
     app.register_blueprint(main_bp)
 
     return app
